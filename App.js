5710cdc1794a351fef12954b2de196962f009afc
Lars Syversen <larsksy@stud.ntnu.no>
2018-10-01 15:06:36 +0200

    #issue18 created header component using react-native-elements

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import React from 'react';
+// import Header from './components/header/Header'
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>gh!</Text>
+        <Header backgroundColor={ '#fff' }  centerComponent={{ text: 'BIRD', style: styles.text }}/>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+      flex: 1,
+      backgroundColor: '#f6f6f6',
+    },
+
+    text: {
+        color: '#ff0042',
+        letterSpacing: 5,
+        fontSize: 20,
+        fontWeight: "bold",
+    },
 });
