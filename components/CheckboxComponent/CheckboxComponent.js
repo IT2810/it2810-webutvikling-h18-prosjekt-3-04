ec15e96fc7900eb5a431c33e550bfa455d3085ee
philipd <philipd@stud.ntnu.no>
2018-10-02 16:24:15 +0200

    Issue#17 - Checkboxes are now done. Aldo updated gitignore to ignore workspace.xml.

--- components/CheckboxComponent/CheckboxComponent.js
+++ components/CheckboxComponent/CheckboxComponent.js
@@ -2,26 +2,13 @@ import React, {Component} from 'react';
 import { View,Text,TouchableHighlight } from 'react-native'
 import styles from "./styles/styles";
 import { Ionicons } from '@expo/vector-icons';
-//import Icon from 'react-native-fa-icons';
-
 
 
 export default class CheckboxComponent extends Component{
-
-    /*
-    componentDidMount() {
-        Font.loadAsync({
-            'FontAwesome': require("../../assets/fonts/fa-solid-900.ttf"),
-        });
-    }
-    */
-
-
     constructor() {
         super();
         this.state= {
             checked: false,
-            checkIcon : ""
         };
     }
 
@@ -30,28 +17,13 @@ export default class CheckboxComponent extends Component{
             checked: !this.state.checked
         });
 
-        if (this.state.checked){
-            this.state.checkIcon = <Ionicons name="md-checkmark-circle" size={32} color="green" />
-        } else {
-            this.state.checkIcon = ""
-        }
-
     };
 
-    render() {
+    render(){
         return (
-            <TouchableHighlight
-                underlayColor={"rgba(0,0,0,0)"}
-                onPress = { this.onPress }
-            >
-                <View
-                    style={[this.state.checked ?  styles.checkboxStyleChecked : styles.checkboxStyle]}
-                >
-                    <Text
-
-                    >
-                        {this.state.checkIcon}
-                    </Text>
+            <TouchableHighlight underlayColor={"rgba(0,0,0,0)"} onPress = { this.onPress }>
+                <View style={styles.checkboxStyle}>
+                    {this.state.checked ? <Ionicons name="md-checkmark" size={35} color="#FF0040" /> : ""}
                 </View>
             </TouchableHighlight>
         );
--- components/CheckboxComponent/styles/styles.js
+++ components/CheckboxComponent/styles/styles.js
@@ -3,21 +3,11 @@ import { StyleSheet } from "react-native"
 
 export default StyleSheet.create({
     checkboxStyle:{
-        height: 50,
-        width: 50,
+        height: 40,
+        width: 40,
         backgroundColor: "#ffffff",
         borderWidth: 3,
         borderColor: "#FF0040",
         borderRadius: 15,
-
-    },
-    checkboxStyleChecked:{
-        height: 50,
-        width: 50,
-        backgroundColor: "#FF0040",
-        borderWidth: 3,
-        borderColor: "#FF0040",
-        borderRadius: 15,
-
-    },
+    }
 })
