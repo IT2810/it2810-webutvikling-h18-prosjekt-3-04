788f639fefd33853398f911f79cbd5a92a725a29
Andreas Ore Larssen <andreasol@me.com>
2018-10-02 17:32:06 +0200

    WIP

--- components/CreateTextTaskViewComponent/CreateTextTaskView.js
+++ components/CreateTextTaskViewComponent/CreateTextTaskView.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, Text,TextInput,Keyboard , View } from 'react-native';
 import styles from './styles/styles'
+import DateTimePicker from 'react-native-modal-datetime-picker';
+
 
 export default class CreateTextTaskView extends React.Component {
+    state = {
+        isDateTimePickerVisible: false,
+    };
 
+    _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+
+
+    _hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
+
+    _handleDatePicked = (date) => {
+        console.log('A date has been picked: ', date);
+        this._hideDateTimePicker();
+    };
     static navigationOptions = {
         title: 'Create Text Task',
         headerStyle: {
@@ -20,7 +34,26 @@ export default class CreateTextTaskView extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text> ÅÅÅÅ DU ER SÅ JÆVLIG TEIT UUUUUUUUG </Text>
+                <TextInput
+                    style ={styles.textInputField}
+                    containerViewStyle={{marginLeft:-10,marginRight:null,width:"105%"}}
+                    placeholder={"Add some descriptive and motivating text to your task! 💪🏼"}
+                    multiline={true}
+                    returnKeyType={"done"}
+                    enablesReturnKeyAutomatically={true}
+                    blurOnSubmit={true}
+                    onSubmitEditing={Keyboard.dismiss}
+                />
+                <TouchableOpacity style={styles.deadlineBtn} onPress={this._showDateTimePicker}>
+                    <Text style={styles.buttonText}>Add deadline +</Text>
+                </TouchableOpacity>
+                <DateTimePicker
+                    isVisible={this.state.isDateTimePickerVisible}
+                    onConfirm={this._handleDatePicked}
+                    onCancel={this._hideDateTimePicker}
+                    is24Hour={true}
+                    mode={"datetime"}
+                />
             </View>
         );
     }
--- components/CreateTextTaskViewComponent/styles/styles.js
+++ components/CreateTextTaskViewComponent/styles/styles.js
@@ -3,9 +3,8 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#f6f6f6',
+        backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
     },
 
     text: {
@@ -14,4 +13,40 @@ export default StyleSheet.create({
         fontSize: 20,
         fontWeight: "bold",
     },
+
+    deadlineBtn: {
+        backgroundColor: '#ff0042',
+        borderRadius:20,
+        borderWidth: 1,
+        borderColor:'#ff0042',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+        elevation: 1,
+    },
+
+    textInputField:{
+        height: 150,
+        borderRadius: 15,
+        borderWidth: 1,
+        borderColor: '#f3f3f3',
+        backgroundColor:'#f3f3f3',
+        //marginBottom:20,
+        padding:10,
+        width: "90%",
+        marginTop:20,
+        marginBottom:20,
+        fontSize: 20,
+        fontWeight:"bold",
+    },
+    buttonText: {
+        fontSize:15,
+        fontWeight: "bold",
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 5,
+        paddingTop:5,
+        color: '#fff',
+    }
 })
