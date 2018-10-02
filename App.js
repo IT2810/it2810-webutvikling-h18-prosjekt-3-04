b58d8696235f37825142c4fcf01c85660d77a8ab
philipd <philipd@stud.ntnu.no>
2018-10-02 14:44:30 +0200

    WIP

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CheckBoxComponent from './components/checkboxComponent/CheckboxComponent'
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>gh!</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>gh!</Text>
+                <CheckBoxComponent/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
