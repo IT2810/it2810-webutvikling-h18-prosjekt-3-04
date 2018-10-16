272ae3983fec696c011865482ccbf6b5f96e5d66
Lars Syversen <larsksy@stud.ntnu.no>
2018-10-17 00:21:26 +0200

    #41 fixed snapshot test bug with rng element

--- components/MotivationalQuoteComponent/__tests__/MotivationalQuoteComponent-test.js
+++ components/MotivationalQuoteComponent/__tests__/MotivationalQuoteComponent-test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MotivationalQuoteComponent from '../MotivationalQuoteComponent';
 import renderer from 'react-test-renderer';
 
-
+Math.random = jest.fn().mockImplementation(() => 1);
 
 test('renders correctly with no elemtents', () => {
     const tree = renderer.create(<MotivationalQuoteComponent numTodos={0}/>).toJSON();
